7a8e989b14bdd2fbf3bfc8f828e9e43c07a54a64
BrightTrump <brightmunemu@gmail.com>
2023-11-05 03:30:52 +0100

    Updated

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [ProductsController::class,'index']);
-Route::get('/add-to-cart/{id}', [ProductsController::class,'addToCart'])->name('add_to_cart');
-Route::get('/cart', [ProductsController::class,'cart'])->name('cart');
-Route::patch('/update-cart', [ProductsController::class,'update'])->name('update_cart');
-Route::delete('/remove-from-cart', [ProductsController::class,'remove'])->name('remove_from_cart');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
